1e40de4bfb391d7f6cf68293d13b1584f2ef056c
zoodogood <52154209+zoodogood@users.noreply.github.com>
2022-07-25 13:05:57 +0300

    Add files via upload

--- js/options.js
+++ js/options.js
@@ -1,25 +1,30 @@
-var options = {
+const options = {
 	min: false,
 	max: false,
 	hide: false
 }
 
 function setupOptions () {
-	var urlParams = new URLSearchParams(document.location.search)
-	for (let option in options) {
-		if (!urlParams.has(option)) {
-			continue
+	const urlParams = new URLSearchParams(document.location.search);
+
+	for (const option in options){
+
+		if (!urlParams.has(option)){
+			continue;
 		}
-		options[option] = urlParams.get(option)
+		options[option] = urlParams.get(option);
 	}
 
+
 	if (options.hide) {
-		pickers[options.hide].style.display = 'none'
+		pickers[options.hide].style.display = 'none';
 	}
+
 	if (options.min) {
-		pickers['date'].setAttribute('min', options.min)
+		pickers['date'].setAttribute('min', options.min);
 	}
+
 	if (options.max) {
-		pickers['date'].setAttribute('max', options.max)
+		pickers['date'].setAttribute('max', options.max);
 	}
 }
